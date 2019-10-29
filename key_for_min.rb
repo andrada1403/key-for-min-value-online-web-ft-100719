19f7c8ba4df7047b95375a9d5e949e0764b056bd
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-10-29 17:54:36 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,6 +7,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |key,value|
     if lowest_value==nil || value<lowest_value
       lowest_value=value
+      key_for_min=key
     end
   end
 end
